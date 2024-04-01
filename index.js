013ae57dde39ac29b894f136c43f841ca876552d
Kevin Mayaka <kevinabuga3@gmail.com>
2024-04-01 12:33:34 +0300

    The JavaScript DOMContentLoaded Event

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded",function(){
+    console.log("DOM Content Has Been Loaded")
+    const myText = document.getElementById("text");
+    myText.innerHTML = "This is really cool!";
+});
+console.log('DOM content not Loaded');
+
